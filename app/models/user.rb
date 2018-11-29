12612bd4c8e67c1baed4a310f16ee30eb4bc4ef2
Tomomi Hatasa <tomomihatasa@MacBook-Pro-3.local>
2018-11-29 10:20:59 +0900

    first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  has_secure_password
+  validates :name, presence: true,  length: { in: 8..35 ,maximum: 255}
+  validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+has_secure_password
 end
