631dc6fc38692e898b369b2fe9e2428252d5dfd5
Tomomi Hatasa <tomomihatasa@MacBook-Pro-3.local>
2018-12-06 19:37:54 +0900

    umakuitteruzyoutai

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
     def new
     end
     def create
-      user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+      user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -31,8 +31,8 @@ private
       session.delete(:user_id)
       @current_user = nil
     end
-  #
-  # def user_params
-  #   params.require(:user).permit(:email)
-  # end
+
+  def session_params
+    params.require(:session).permit(:email,:password)
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,9 +8,8 @@ class Topic < ApplicationRecord
   belongs_to :user
   has_many :topics
   mount_uploader :image, ImageUploader
-  validates :image
-    file_size: {
-      maximum: 10.megabytes.to_i # 最大10MBに制限
-    }
+    # file_size: {
+    #   maximum: 10.megabytes.to_i # 最大10MBに制限
+    # }
     has_many :favorites
 end
